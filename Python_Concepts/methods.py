9e2a49c28bf8ae36f5122029ddab84b0c7d87bf7
Abhishek Gowda H C <abhishek@autocrat.local>
2025-08-11 04:24:10 -0700

    Completed with Methods in Python

---
+++ Python_Concepts/methods.py
@@ -0,0 +1,62 @@
+# Static Methods
+
+# These  Methods behave like normal functions Inside the Class .
+
+class Company:
+
+    @staticmethod
+    def Shope_Floor(a,b):
+        return f"These are machines in shopeFloor {a}:{b}"
+    
+C = Company()  #Using Object
+
+print(C.Shope_Floor(1,2)) 
+    
+print(Company.Shope_Floor("Mazzak","Tusnami"))   #Using Class
+
+# Class Method It will inhert the from base class and take the Varible reference form the sub class if 
+# present or It will refer to Base Class Varible.
+
+class Dog:
+   
+    Bark = "Bow Bow..."
+
+    @classmethod
+    def Make_sound(cls):
+        return cls.Bark
+    
+class PitBull(Dog):
+    
+    Bark = "Bow Bow Bow ....."
+
+print(Dog.Make_sound())
+print(PitBull.Make_sound())
+
+# Factory Method
+
+class Birday:
+
+    def __init__(self,name,age):
+        self.UserName = name
+        self.UserAge = age
+    
+    @classmethod
+    def BirthdayUser(cls,Name,Birthday_Year):
+        current_Year = 2025
+        age = current_Year - Birthday_Year
+        return cls(Name,age)
+
+P1 = Birday("yogesh",30)
+P2 = Birday.BirthdayUser("bhrath",2000)
+
+print(P1.UserName,P1.UserAge)
+print(P2.UserName,P2.UserAge)
+
+
+"""he provided code defines a Person class with an __init__ method (the standard constructor)
+ and a class method from_birth_year (an alternative or "custom" constructor).
+ It then creates two instances: p1 using the standard constructor and p2 using the custom class method.
+ """
+# then returns a new instance by calling cls(name, age)—which is equivalent to Person(name, age). 
+# This doesn't create a "custom instance" type;
+#  it's just a convenient alternative way to instantiate the class without directly providing age.

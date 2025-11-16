d4897c7716714dc07abe748fbf674f32baea756d
AB_GOWDA <abhishekgowda8090@gmail.com>
2025-11-16 13:39:07 +0530

    Numpy Intial Commit

---
+++ Numpy/main.py
@@ -0,0 +1,19 @@
+# Practise Numpy in Python
+import numpy as np
+
+print(np.__version__)
+
+my_list = [1,2,3,4]
+
+# Conveting the List into Array
+my_aarry = np.array(my_list)
+print(type(my_aarry))
+
+# Generating the Matrix with All Pre-Defined Values or intial Empty Array
+zero_matrix = np.zeros((3,3))
+print(zero_matrix)
+
+# Pre-Defined one Matrix
+oneDMatrix = np.ones((3,3))
+print(oneDMatrix)
+

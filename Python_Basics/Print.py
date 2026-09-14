cc1aec5c857176fc25c62653fee87dde697f497f
Abhishek Gowda H C <abhishekgowda8090@gmail.com>
2026-09-14 21:49:00 +0530

    Last commit of Day .. Happy Ganesh Chaturthi

--- Python_Basics/Print.py
+++ Python_Basics/Print.py
@@ -9,3 +9,21 @@ print(7.8)
 
 print("hello",1,1.6,True,sep="/")
 print("hello",1,1.6,True,sep="*")
+
+name = 'mamau'
+
+print(f"hello my name is {name}")
+
+a = "Abhi"
+b = "Shravana"
+print(a+" "+b)
+
+num = 1234.12234
+print(f"the number is {num:.2f}")
+
+print("line_1\nline_2\nline_3")
+
+print("""hello
+my name is abhi
+I am learning python because i want to become 
+top software engineers in the worls""")

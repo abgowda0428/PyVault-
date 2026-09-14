20454a69eed01453b7369be0416326844cc321e1
Abhishek Gowda H C <hcabhishekgowda@gmail.com>
2026-09-14 20:22:57 +0530

    Back Online

--- Python_Basics/Print.py
+++ Python_Basics/Print.py
@@ -2,3 +2,10 @@
 
 print("Hello World")
 print("Tday I am starting Python")
+
+print("Hi My name is Abhi")
+print (7)
+print(7.8)
+
+print("hello",1,1.6,True,sep="/")
+print("hello",1,1.6,True,sep="*")
